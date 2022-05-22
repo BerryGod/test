7deca4b87e6d7f382e1d08218d6eab4a39562583
BerryGod <84345559+BerryGod@users.noreply.github.com>
2022-05-22 20:54:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-import AutoGitUpdate from 'auto-git-update';
-
-const config = {
-    repository: 'https://github.com/chegele/BackupPurger'
-    tempLocation: 'C:/Users/scheg/Desktop/tmp/',
-    ignoreFiles: ['util/config.js'],
-    executeOnComplete: 'C:\\Users\\scheg\\Desktop\\worksapce\\AutoGitUpdate\\startTest.bat',
-    exitOnComplete: true
-}
-
-const updater = new AutoGitUpdate(config);
-
-updater.autoUpdate();
+const aufg = require('auto-update-from-github');
+ 
+aufg({
+    git: 'BerryGod/test', // 远程git地址
+    dir: './repository', // 本地路径
+    type: 'version', // 检测类型 version | commit
+    freq: 3000 // 刷新频率
+});
 
 function print(text) {
     console.log(text)
