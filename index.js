f03e9b009eff5bda3111dc99366375a1b109267e
BerryGod <84345559+BerryGod@users.noreply.github.com>
2022-05-22 20:41:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import AutoGitUpdate from 'auto-git-update';
+
+const config = {
+    repository: 'https://github.com/chegele/BackupPurger'
+    tempLocation: 'C:/Users/scheg/Desktop/tmp/',
+    ignoreFiles: ['util/config.js'],
+    executeOnComplete: 'C:\\Users\\scheg\\Desktop\\worksapce\\AutoGitUpdate\\startTest.bat',
+    exitOnComplete: true
+}
+
+const updater = new AutoGitUpdate(config);
+
+updater.autoUpdate();
+
 function print(text) {
     console.log(text)
 }
